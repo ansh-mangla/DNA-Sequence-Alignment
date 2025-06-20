368bb4d89cb9535237272839b9b336fca5638fc4
ansh mangla <mangla24ansh@gmail.com>
2025-06-20 16:25:52 +0530

    minor correction

--- test.py
+++ test.py
@@ -3,26 +3,37 @@ from Bio import SeqIO
 from Bio import Align
 from Bio.Seq import Seq
 
+# sequnces
+seq1 = str(SeqIO.read("data/COX1 Human CDS.txt", 'fasta').seq)
+seq2 = str(SeqIO.read("data/COX1 Mouse CDS.txt", 'fasta').seq)
+mode = "global"
+# my function
+scheme = {
+    "match": 1,
+    "mismatch": -1,
+    "gap": -2
+}
+my_align = pairwise_alignment(
+    seq1, seq2, mode=mode, align=False, scheme=scheme)
 
-huamn = str(SeqIO.read("data/COX1 Human CDS.txt", 'fasta').seq)
-yeast = str(SeqIO.read("data/COX1 Mouse CDS.txt", 'fasta').seq)
+# biopython
+seq1 = Seq(seq1)
+seq2 = Seq(seq2)
+
+aligner = Align.PairwiseAligner()
+aligner.mode = mode
+
+aligner.match_score = 1
+aligner.mismatch_score = -1
+aligner.open_gap_score = -2
+aligner.extend_gap_score = -2
+
+alignments = aligner.align(seq1, seq2)
+
+print(alignments[0].score)
 
 
-my_align = pairwise_alignment(huamn, yeast, mode="global")
+# comparing scores
 
-
-# seq1 = Seq(huamn)
-# seq2 = Seq(yeast)
-
-# aligner = Align.PairwiseAligner()
-# aligner.mode = "local"
-
-# aligner.match_score = 1
-# aligner.mismatch_score = -1
-# aligner.open_gap_score = -2
-# aligner.extend_gap_score = -2
-
-# alignments = aligner.align(seq1, seq2)
-
-# print(alignments[0])
-# print(alignments[0].score)
+if my_align["score"] == alignments[0].score:
+    print("Result matched.")
